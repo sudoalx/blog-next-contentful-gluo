be698883d31f587e40dc3443fe70129b62d5add9
Alex <alex@alex-dev.me>
2024-04-08 13:21:30 -0600

    Add draft mode support and disable-draft route

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -1,7 +1,19 @@
+import { draftMode } from "next/headers";
+import ExitDraftModeLink from "../components/preview/ExitDraftModeLink";
+
 export default function BlogLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  return <>{children}</>;
+}>) {
+  return (
+    <>
+      {draftMode().isEnabled && (
+        <p className="bg-orange-200 py-4 px-[6vw]">
+          Draft mode is on! <ExitDraftModeLink className="underline" />
+        </p>
+      )}
+      {children}
+    </>
+  );
 }
--- src/contentful/contentImage.ts
+++ src/contentful/contentImage.ts
@@ -23,9 +23,9 @@ export function parseContentfulContentImage(
   }
 
   return {
-    src: asset.fields.file?.url || "",
-    alt: asset.fields.description || "",
-    width: asset.fields.file?.details.image?.width || 0,
-    height: asset.fields.file?.details.image?.height || 0,
+    src: asset.fields.file?.url ?? "",
+    alt: asset.fields.description ?? "",
+    width: asset.fields.file?.details.image?.width ?? 0,
+    height: asset.fields.file?.details.image?.height ?? 0,
   };
 }
