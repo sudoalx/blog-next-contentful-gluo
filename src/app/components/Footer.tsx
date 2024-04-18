405055e998524de83bd856ca7934b1a20734d49d
Alex <alex@alex-dev.me>
2024-04-18 12:27:44 -0600

    Update Footer component to include social media icons and contact information

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,47 @@
-import Image from "next/image";
+import Link from "next/link";
 import { Logo } from "./branding/Logo";
+import { BiPhoneCall } from "react-icons/bi";
+import { FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
-    <div className="text-center p-4 bg-gray-100">
-      <div className="flex justify-center items-center">
-        <Logo />
+    <footer className="bg-gray-100">
+      <div className="container mx-auto py-8 flex justify-between items-center flex-col md:flex-row gap-6 text-center md:text-left">
+        <div>
+          <h4 className="text-xl mb-4">Follow us on</h4>
+          <div className="mb-4 flex items-center gap-4">
+            <FaInstagram className="inline-block text-3xl text-red-500" />
+            <FaLinkedinIn className="inline-block text-3xl text-blue-500 " />
+            <FaYoutube className="inline-block text-3xl text-red-500 " />
+          </div>
+        </div>
+
+        <div className="text-base">
+          <h4 className="text-xl">Meet us at</h4>
+          <p>
+            <b>CDMX:</b> WeWork, Miguel de Cervantes Saavedra 169, Granada,
+            11520, Miguel Hidalgo.
+          </p>
+          <p>
+            <b>Monterrey:</b> WeWork, Av. Constitución 2050 Floor 38, 64000. MX
+          </p>
+
+          <p className="my-2  hover:text-gray-600 transition duration-300 ease-in-out">
+            <BiPhoneCall className="inline-block" />
+            <a href="tel:+525541635960" className="ml-2">
+              <b>+52 55 4163 5960</b>
+            </a>
+          </p>
+
+          <p className="text-gray-600">Made with ❤️ in MTY</p>
+        </div>
+
+        <div>
+          <Link href={"/"}>
+            <Logo />
+          </Link>
+        </div>
       </div>
-      Copyright &copy; 2024 - Gluo Developers
-    </div>
+    </footer>
   );
 };
