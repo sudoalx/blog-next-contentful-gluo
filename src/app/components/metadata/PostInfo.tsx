4f957bcf5b9a9747e62694fad294d4e7a651b457
Alex <alex@alex-dev.me>
2024-04-17 23:43:34 -0600

    Refactor PostInfo component to add author link and image support

--- src/app/components/metadata/PostInfo.tsx
+++ src/app/components/metadata/PostInfo.tsx
@@ -1,6 +1,8 @@
 import { Document as RichTextDocument } from "@contentful/rich-text-types";
 import { fetchAuthorProfileById } from "@/contentful/lib/authorProfile";
 import { readingTimeEstimator } from "@/lib/utils/reading-time";
+import Link from "next/link";
+import ContentfulImage from "../ui/ContentfulImage";
 
 interface PostInfoProps {
   creationDate: Date;
@@ -16,11 +18,24 @@ export const PostInfo = async ({
   const author = await fetchAuthorProfileById(authorId);
   const readingTime = await readingTimeEstimator(body);
   return (
-    <div className="flex flex-wrap gap-2 sm:gap-10 mt-4 text-xs text-gray-500">
+    <div className="flex flex-wrap gap-4 sm:gap-10 mt-4 text-xs text-gray-500 items-center">
       <p className="text-xs text-gray-500">
-        {creationDate?.toLocaleDateString("en-GB")}
+        <time dateTime={creationDate?.toISOString()}>
+          {creationDate?.toLocaleDateString("en-GB")}
+        </time>
       </p>
-      <p className="text-xs text-gray-500">Author: {author.fullName}</p>
+      <div className="flex items-center text-xs text-gray-500">
+        <Link
+          href={`/author/${author.slug}`}
+          className="flex items-center text-xs text-gray-500"
+        >
+          <ContentfulImage
+            {...author.photo!}
+            className="rounded-full h-6 w-6 mr-2"
+          />
+          {author.fullName}
+        </Link>
+      </div>
       <p className="text-xs text-gray-500">{`${readingTime}`}</p>
     </div>
   );
