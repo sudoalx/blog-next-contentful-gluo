7581e01acc1b4d5b6c776adae7cdfc8345f54d48
Alex <alex@alex-dev.me>
2024-04-18 12:27:37 -0600

    Refactor page.tsx to improve post layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./components";
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto my-10">
       {/* Grid of articles and sidebar */}
       <div className="p-4 flex flex-col lg:flex lg:flex-row gap-2">
         <Grid />
