bb8b6b4c1c355fae5da54536ec936942a27f79dd
Alex <alex@alex-dev.me>
2024-04-19 08:48:44 -0600

    Add language switch pill to Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Logo } from "./branding/Logo";
 import { asm } from "../config/fonts";
+import { GrLanguage } from "react-icons/gr";
 
 export const Navbar = () => {
   return (
@@ -12,22 +13,32 @@ export const Navbar = () => {
         >
           <Logo />
         </Link>
-        <ul className="hidden space-x-4 sm:flex">
-          <li>
-            <Link href="#" className={`${asm.className}`}>
+        <ul className="hidden space-x-4 items-center sm:flex">
+          <li className="px-5 py-4">
+            <Link href="#" className={`${asm.className} text-base`}>
               Home
             </Link>
           </li>
-          <li>
-            <Link href="#" className={`${asm.className}`}>
+          <li className="px-5 py-4">
+            <Link href="#" className={`${asm.className} text-base`}>
               About
             </Link>
           </li>
-          <li>
-            <Link href="#" className={`${asm.className}`}>
+          <li className="px-5 py-4">
+            <Link
+              href="#"
+              className={`${asm.className} text-base bg-[#D2FC51] px-6 py-4 rounded-lg`}
+            >
               Contact
             </Link>
           </li>
+          {/* Language switch pill */}
+          <button
+            className={`px-4 py-2 h-8 bg-[#F7F7F7] rounded-full flex items-center text-sm ${asm.className}`}
+          >
+            <GrLanguage className="inline mr-2" />
+            EN
+          </button>
         </ul>
       </div>
     </nav>
