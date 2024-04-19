11912b7bbcb67f847d46648013e0f34518b5cc20
Alex <alex@alex-dev.me>
2024-04-19 08:48:35 -0600

    Add category page with sidebar and grid layout

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,15 +1,43 @@
+import { Sidebar } from "@/app/components";
+import { Grid } from "@/app/components/layout/PostsGrid";
+import { einaLight } from "@/app/config/fonts";
+import { fetchBlogPosts } from "@/contentful/lib/blogPosts";
+import { draftMode } from "next/headers";
+import Link from "next/link";
+import { SlArrowRight } from "react-icons/sl";
+
 interface CategoriesPageProps {
   params: {
-    categories: string;
+    category: string;
   };
 }
 
-export default function CategoriesPage({
+export default async function CategoriesPage({
   params,
 }: Readonly<CategoriesPageProps>) {
+  const { category } = params;
+  const blogPosts = await fetchBlogPosts({ preview: draftMode().isEnabled });
   return (
-    <div>
-      <h1>Posts with the category: {params.categories}</h1>
-    </div>
+    <main className="container mx-auto mb-10">
+      {/* Blog category page */}
+      {/* Blog category title */}
+      <div className={`${einaLight.className} my-10 text-4xl`}>
+        <Link
+          href="/"
+          className="mx-4 hover:text-blue-600"
+          aria-label="Back to blog"
+        >
+          Blog
+        </Link>
+        <SlArrowRight className="inline text-2xl" />
+        <span className="mx-4 capitalize">{decodeURI(category)}</span>
+      </div>
+      <div className="p-4 flex flex-col lg:flex lg:flex-row gap-2">
+        {/* Grid of articles */}
+        <Grid blogPosts={blogPosts} />
+        {/* Sidebar */}
+        <Sidebar />
+      </div>
+    </main>
   );
 }
