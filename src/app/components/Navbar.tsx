7d4c980da4423b98bfd92f8898f9ae60149f5789
Alex <alex@alex-dev.me>
2024-04-24 19:42:04 -0600

    Refactor Navbar component to include responsive menu in Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,13 +7,16 @@ export const Navbar = () => {
   return (
     <nav className="p-4">
       <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
         <Link
           href="/"
           className="text-2xl font-bold flex justify-center items-center"
         >
           <Logo />
         </Link>
-        <ul className="hidden space-x-4 items-center sm:flex">
+
+        {/* Site Menu */}
+        <ul className="hidden space-x-4 items-center md:flex">
           <li className="px-5 py-4">
             <Link href="#" className={`${asm.className} text-base`}>
               Home
@@ -40,6 +43,24 @@ export const Navbar = () => {
             EN
           </button>
         </ul>
+        <div className="md:hidden">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </nav>
   );
