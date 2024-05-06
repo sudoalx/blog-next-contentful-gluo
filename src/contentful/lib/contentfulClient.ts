4444f5d33d5373cdfefba1b255591eccacf7e9d5
Alex <alex@sudoalex.dev>
2024-05-06 09:53:43 -0600

    Refactor imports and update Contentful client configuration

--- src/contentful/lib/contentfulClient.ts
+++ src/contentful/lib/contentfulClient.ts
@@ -1,15 +1,15 @@
 import { createClient } from "contentful";
 
 const {
-  NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-  NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  CONTENTFUL_SPACE_ID,
+  CONTENTFUL_ACCESS_TOKEN,
+  CONTENTFUL_PREVIEW_ACCESS_TOKEN,
 } = process.env;
 
 if (
-  !NEXT_PUBLIC_CONTENTFUL_SPACE_ID ||
-  !NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ||
-  !NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN
+  !CONTENTFUL_SPACE_ID ||
+  !CONTENTFUL_ACCESS_TOKEN ||
+  !CONTENTFUL_PREVIEW_ACCESS_TOKEN
 ) {
   throw new Error("Contentful environment variables are not set");
 }
@@ -17,15 +17,15 @@ if (
 // This is the standard Contentful client. It fetches
 // content that has been published.
 const client = createClient({
-  space: NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space: CONTENTFUL_SPACE_ID,
+  accessToken: CONTENTFUL_ACCESS_TOKEN,
 });
 
 // This is a Contentful client that's been configured
 // to fetch drafts and unpublished content.
 const previewClient = createClient({
-  space: NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  space: CONTENTFUL_SPACE_ID,
+  accessToken: CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   host: "preview.contentful.com",
 });
 
